0494ef5ca0ffe662ac0e1289b03ea386e2445697
baptistapedro <pedrojorgesbaptista@gmail.com>
2022-10-08 04:43:02 +0200

    Update fuzz.go

--- harnesses/fuzz.go
+++ harnesses/fuzz.go
@@ -5,23 +5,9 @@ import (
 	"github.com/davidbyttow/govips/v2/vips"
 )
 
-func checkError(err error) {
-	if err != nil {
-		return
-	}
-}
-
 func main() {
 	vips.Startup(nil)
 	defer vips.Shutdown()
 
-	image1, err := vips.NewImageFromFile(os.Args[1])
-	checkError(err)
-
-	// Rotate the picture upright and reset EXIF orientation tag
-	err = image1.AutoRotate()
-	checkError(err)
-
-	//ep := vips.NewDefaultJPEGExportParams()
-	//image1.Export(ep)
+	vips.NewImageFromFile(os.Args[1])
 }
